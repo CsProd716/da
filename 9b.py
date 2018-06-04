4c8832c533765e571651c924a15e2d9ed6c4ae6d
CsProd716 <39880093+CsProd716@users.noreply.github.com>
2018-06-04 17:41:30 +0000

    Add files via upload

---
+++ 9b.py
@@ -0,0 +1,24 @@
+class student:
+    count = 0
+    #pycount = 0
+    def __init__(self,name,usn,subject):
+        self.name = name
+        self.usn = usn
+        self.subject = subject
+        #if self.subject.lower == "python":
+            #student.pycount+=1
+        student.count+=1
+studlist = []
+while(1):
+    i = str(input("Enteries done"))
+    if i.lower() == "yes":
+        break
+    name = input("Enter name")
+    usn = input("Enter usn")
+    subject = input("Enter subject")
+    r = student(name,usn,subject)
+    studlist.append(r)
+for i in studlist:
+    if i.subject.lower() == "python":
+        print(i.name,"has opted for Python")
+print("Number of students in class:",student.count)
