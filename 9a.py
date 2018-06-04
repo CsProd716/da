133e101839c92e8f0864ecbf4a95edc8e54006f8
CsProd716 <39880093+CsProd716@users.noreply.github.com>
2018-06-04 17:05:02 +0000

    Add files via upload

---
+++ 9a.py
@@ -0,0 +1,13 @@
+def bubble(*l):
+    n = len(l)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if l[j]>l[j+1]:
+                l[j],l[j+1] = l[j+1],l[j]
+    return l
+
+x = bubble([int(x) for x in input.split()])
+print("Sorted list:")
+print(x)
+
+
