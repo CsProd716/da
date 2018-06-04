54f01ce7a5f48516b5cdbaac24428bffc0c4863a
CsProd716 <39880093+CsProd716@users.noreply.github.com>
2018-06-04 16:17:12 +0530

    Create 3a.py

---
+++ 3a.py
@@ -0,0 +1,65 @@
+def add(dic):
+    k = str(input("Enter the phone "))
+    v = float(input("Enter the price "))
+    d1 = {k:v}
+    dic.update(d1)
+    return
+
+def retrive(dic):
+    s = str(input("Enter the phone"))
+    print("The price of ",s,":", dic[s])
+    return
+
+def same(dic):#has to be corrected
+    new = {}
+    for k,v in dic.iteritems():
+        new.setdefault(v,[]).append(k)
+    print(new)
+        #print(v,"--->",k)
+    return
+
+def rem(dic):
+    s = str(input("Give in the Phone"))
+    if s in dic:
+        del dic[s]
+        print(s,"deleted from the dictionary")
+    else:
+        print("The phone not found in dictionary")
+    return
+
+def sort(dic):
+    #print(sorted(dic.values()))
+    print(sorted(dic,key = dic.__getitem__))
+    return    
+
+dic = {}
+
+while(1):
+    print("MENU")
+    print("0.Show Dictionary")
+    print("1.ADD a new entry")
+    print("2.Search for info of one phone")
+    print("3.Find phones with same price")
+    print("4.Remove an entry")
+    print("5.Display a price sorted list")
+    print("9.EXIT")
+    i = int(input("Choose the operation"))
+    if i==0:
+        print(dic.items())
+    elif i==1:
+        add(dic)
+    elif i==2:
+        retrive(dic) 
+    elif i==3:
+        same(dic)
+    elif i==4:
+        rem(dic)
+    elif i==5:
+       sort(dic)
+    elif i==9:
+        print("Exiting...\n")
+        exit()
+    
+
+              
+    
